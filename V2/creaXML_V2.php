4490ce697333eab0e94815024549f06a9637982d
Pedro Espigares <98234152+pedroespigares@users.noreply.github.com>
2022-10-19 17:29:44 +0200

    Add files via upload

---
+++ V2/creaXML_V2.php
@@ -0,0 +1,54 @@
+<?php
+	echo "<h1>Crea aquí tu archivo XML</h1>";
+	echo "<br />";
+	echo "<b>Consideraciones a tener en cuenta:</b>";
+	echo "<ul>";
+	echo "<li>Puede no haber profesores asignados a una asignatura. En ese caso no se crea el nodo correspondiente.</li>";
+	echo "<li>RECORDATORIO php: Si no hay profes, el array no existe. Que no es lo mismo a que esté vacío. Usa isset.</li>";
+	echo "<li>Guarda el fichero en la misma carpeta del script con el nombre <i>asignaturas.xml</i>. Después, lo muestras en el navegador</li>";
+	echo "<li><b>Versión 1.0</b>: Crea un archivo XML nuevo por cada ejecución del script.</li>";
+	echo "<li><b>Versión 2.0</b>: Ir acumulando asignaturas en el XML cuando se llame al script. Para ello, deberás comprobar si existe el archivo para abrirlo, cargarlo en un objeto simpleXML y añadir la nueva asignatura.</li>";
+	echo "</ul>";
+	echo "<a href='#' onclick='history.go(-1);'>Ir atrás</a>";
+
+    function añadirAsignatura(){
+        if(file_exists('xmlAsignaturas.xml')){
+            $centro = simplexml_load_file('xmlAsignaturas.xml');
+            $centro = new SimpleXMLElement(file_get_contents('xmlAsignaturas.xml'));
+        }
+
+        $asignatura = $centro -> addChild("asignatura");
+        $asignatura -> addChild("codigo",$_POST["codigo"]);
+        $asignatura -> addChild("nombre",$_POST["nombre"]);
+        $asignatura -> addChild("duracion",$_POST["duracion"]);
+        $asignatura -> addChild("tipo",$_POST["tipo"]);
+
+        $teachers = $asignatura -> addChild("profesores");
+        $titulares = $teachers -> addChild("titulares");
+        $practicas = $teachers -> addChild("practicas");
+        
+
+        if(isset($_POST["titDNI"]) && isset($_POST["titNombre"]) && isset($_POST["titApellidos"])){
+            for($i = 0; $i < count($_POST["titDNI"]); $i++){
+                $singleTitularTeacher = $titulares -> addChild("profesor");
+                $singleTitularTeacher -> addChild("DNI",$_POST["titDNI"][$i]);
+                $singleTitularTeacher -> addChild("nombre",$_POST["titNombre"][$i]);
+                $singleTitularTeacher -> addChild("apellidos",$_POST["titApellidos"][$i]);
+            }
+        }
+
+        if(isset($_POST["pracDNI"]) && isset($_POST["pracNombre"]) && isset($_POST["pracApellidos"])){
+            for($i = 0; $i < count($_POST["pracDNI"]); $i++){
+                $singlePracticeTeacher = $practicas -> addChild("profesor");
+                $singlePracticeTeacher -> addChild("DNI",$_POST["pracDNI"][$i]);
+                $singlePracticeTeacher -> addChild("nombre",$_POST["pracNombre"][$i]);
+                $singlePracticeTeacher -> addChild("apellidos",$_POST["pracApellidos"][$i]);
+            }
+        }
+
+        file_put_contents("xmlAsignaturas.xml",$centro->asXML());
+        header("Location: xmlAsignaturas.xml");
+    }
+
+    añadirAsignatura();
+?>
